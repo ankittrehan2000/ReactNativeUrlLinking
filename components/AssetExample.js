3f1e01520e7bd13ee3aa09002e5485a47f0c1c29
ankittrehan2000 <47463734+ankittrehan2000@users.noreply.github.com>
2020-01-29 11:25:47 -0500

    Create AssetExample.js

--- components/AssetExample.js
+++ components/AssetExample.js
@@ -1,1 +1,39 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image,Linking } from 'react-native';
 
+export default class AssetExample extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          Article{"\n"} 
+          <Text style={styles.text} onPress={()=>Linking.openURL('http://google.com')}>Link</Text>
+        </Text>
+        <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
+        <Text style={{fontStyle:'Oblique'}}> Description </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  logo: {
+    height: 150,
+    width: 128,
+  },
+  text: {
+    color: 'blue',
+  }
+});
